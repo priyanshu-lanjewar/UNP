42ecc1745192972c5d03f50fee80c48b97e73ac4
priyanshu-lanjewar <priyanshu.lanjewar@yahoo.com>
2022-05-19 08:24:24 +0530

    comments

--- ECHO/EchoServer.c
+++ ECHO/EchoServer.c
@@ -15,14 +15,20 @@ int main(int argc, char *argv[])
     server_address.sin_port = htons(5900);
     server_address.sin_addr.s_addr = inet_addr(argv[1]);
 
+
     bind(net_socket, (struct sockaddr*)&server_address,sizeof(server_address));
 
+
+    // Listen Function creates Queue for incoming Request
+    // Maintain 2 Queue 1 for incomplete 3 way handshake and other for those who has completed 3 way handshake
     listen(net_socket,10);
     char str[100];
     int fd;
     int c;
     while(1)
     {
+        // accepts the request from completed queue
+        // accept is blocking function: if there is no incoming request accept function will pause execution of program further unless it encounter any request
         fd=accept(net_socket,(struct sockaddr*)&client_address,&c);
         while (1) {
             int n = recv(fd, str, 99, 0);
