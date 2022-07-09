303e36659a4c44988456f057e9aafc80699968a2
priyanshu-lanjewar <priyanshu.lanjewar@yahoo.com>
2022-07-09 08:56:09 +0530

    udp-echo-alarm

--- UDP ECHO/client.c
+++ UDP ECHO/client.c
@@ -2,9 +2,16 @@
 #include "arpa/inet.h"
 #include "sys/socket.h"
 #include "unistd.h"
+#include "signal.h"
+
+void timeout(int signal){
+    printf("Timeout Occurred\nNo Response\nExiting....");
+    _exit(0);
+}
 
 int main()
 {
+    signal(SIGALRM,timeout);
     int sock = socket(AF_INET,SOCK_DGRAM,0);
     if(sock<0)
         perror("Failed Creating Socket : ");
@@ -24,28 +31,24 @@ int main()
 
 
     int len = sizeof (struct sockaddr_in);
-    int cr = connect(sock,(struct sockaddr*)&add,len);
-    struct sockaddr_in sa;
-
-    if(cr<0)
-        perror("Connection Failed ..");
-    else{
-        getpeername(sock,(struct sockaddr*)&sa,&len);
-        printf("Server IP = %s\n", inet_ntoa(sa.sin_addr));
-        printf("Server Port = %d\n", ntohs(sa.sin_port));
-    }
     int n;
     char buf[200];
 
     while (fgets(buf,200,stdin)){
-        n = write(sock,buf,200);
+        len  = sizeof (struct sockaddr_in);
+        n = sendto(sock,buf,200,0,(struct sockaddr*)&add,len);
         if (n<0)
             perror("Error While Writing : ");
-        n = read(sock,buf,200);
+        alarm(5);
+        len  = sizeof (struct sockaddr_in);
+        n = recvfrom(sock,buf,200,0,(struct sockaddr*)&add,&len);
         if (n<0)
             perror("Error While Reading : ");
-        buf[n] = '\0';
-        printf("Data Recieved : %s",buf);
+        else{
+            alarm(0);
+            buf[n] = '\0';
+            printf("Data Recieved : %s",buf);
+        }
     }
     close(buf);
     return  0;
